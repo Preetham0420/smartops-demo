b8ed9dfbfdec53cf94a7f6afe6a6eef24e2c70c7
Preetham Elakurthy <preethamelakurthy@gmail.com>
2025-12-21 23:39:13 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,9 +2,9 @@
     return a + b
 
 def test_add():
-    assert add_numbers(2, 3) == 5
+    assert add_numbers(2, 3) == 6
     assert add_numbers(0, 0) == 0 # THIS WILL FAIL!
-    assert add_numbers(-1, 1) == 40
+    assert add_numbers(-1, 1) == 0
     assert add_numbers(10, 5) == 15  
     print("All tests passed!")
 
